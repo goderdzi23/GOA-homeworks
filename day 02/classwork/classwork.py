07cdcd9d9f137eb1b2665f07d2a4d9419e730242
goderdzi23 <dzamukashviligode@gmail.com>
2025-07-09 18:15:19 +0400

    commit

---
+++ day 02/classwork/classwork.py
@@ -0,0 +1,11 @@
+age = 12
+name = "gode"
+print(name, "არის", str( age ), "წლის") 
+
+
+name = "davit"
+age = 21
+group = 3
+gio = '"'
+salome = "4"
+salolomi = "55 wlis aris"
---
+++ day 02/homework/homework.py
@@ -0,0 +1,25 @@
+name = "ლუკას"
+birthday = 2015
+print("მოგეალმებით", name,"დაბადების დღე გაქვს", birthday)
+# ბრჭყალები არი იყო (ლუკა რო ეწერა) birthday-ში ტოლობის ნიშნის გარდა მეტობის იყო
+# და ბოლოს ბრჩხილი არ იყო დახურული და სიტყვდა "დაბადების" უკან არ იყო ბრჭყალი
+
+
+score = 250
+print(score)
+
+
+health = 100
+print(health)
+
+
+mom = "sofo fifia"
+dad = "mickheil dzamukashvili"
+sister = "natia dzamukashvili"
+print(mom, dad, sister)
+
+
+# concatination  / კონკატინაცია ნიშნავს string ების შეერთებას
+x = 25
+name = "გოდე"
+print(name + "არის" + str( x ) + "წლის")
